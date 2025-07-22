24db32e4b239cec5731cb34dbbe9b5959a107cc6
myrza218 <myrzasyrgabekov28@gmail.com>
2025-07-22 17:44:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const optionsContainer = document.querySelector('.options');
+  const options = document.querySelectorAll('.option');
+
+  optionsContainer.style.setProperty('--total-options', options.length);
+
+  optionsContainer.addEventListener('click', (event) => {
+    const clickedOption = event.target.closest('.option');
+
+    if (!clickedOption || clickedOption.classList.contains('active')) return;
+
+    options.forEach((option) => {
+      option.classList.remove('active');
+    });
+
+    clickedOption.classList.add('active');
+  });
+});
